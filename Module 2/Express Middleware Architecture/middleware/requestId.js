9ba25e4dec3895af04209beb91ccd9f24cf5a29f
nannam sandeep <nannam.kumar.s.129@kalvium.community>
2026-08-28 09:46:57 +0000

    feat: implement express middleware observability

--- Module 2/Express Middleware Architecture/middleware/requestId.js
+++ Module 2/Express Middleware Architecture/middleware/requestId.js
@@ -11,5 +11,7 @@
 const { randomUUID } = require('crypto');
 
 module.exports = function requestId(req, res, next) {
-  // TODO: implement the four steps described above.
+  req.id = randomUUID();
+  res.setHeader('X-Request-Id', req.id);
+  next();
 };
